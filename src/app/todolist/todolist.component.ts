6e12359f37d4bc8b1a1fee4f77a08dbb248e1f92
Oumagogah70 <oumagogah70@gmail.com>
2024-07-26 17:29:15 +0300

    updated the error massage display and made data storage on local

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,32 +1,52 @@
-import { Component } from '@angular/core';
-import { JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl,FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-todolist',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule,JsonPipe],
+  imports: [ReactiveFormsModule, CommonModule, JsonPipe],
   templateUrl: './todolist.component.html',
-  styleUrl: './todolist.component.css'
+  styleUrls: ['./todolist.component.css']
 })
-export class TodolistComponent {
-  
+export class TodolistComponent implements OnInit {
+
   taskForm: FormGroup = new FormGroup({
-    taskEntry: new FormControl("",[Validators.required]),
-    dateCompleted: new FormControl("",[Validators.required])
-  })
+    taskEntry: new FormControl("", [Validators.required]),
+    dateCompleted: new FormControl("", [Validators.required])
+  });
 
   tasks: any[] = [];
+  submitted = false;
+
+  ngOnInit() {
+    this.loadTasksFromLocalStorage();
+  }
 
   onSave() {
+    this.submitted = true;
     if (this.taskForm.valid) {
       this.tasks.push(this.taskForm.value);
+      this.saveTasksToLocalStorage();
       this.taskForm.reset();
+      this.submitted = false;
     }
   }
 
   onDelete(index: number) {
     this.tasks.splice(index, 1);
+    this.saveTasksToLocalStorage();
+  }
+
+  private loadTasksFromLocalStorage() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
+  private saveTasksToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
-
